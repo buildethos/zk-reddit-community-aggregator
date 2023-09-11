3d8c331b81083d692d339c8ef19cd713ef9a62ec
mxnares <69873782+mxnares@users.noreply.github.com>
2023-09-11 10:47:52 -0700

    feat add: retrieve user's top subreddits

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,27 @@ import passport from "passport";
 import session from "express-session";
 import { Strategy as RedditStrategy } from "passport-reddit";
 import dotenv from "dotenv";
+import fetch from "node-fetch";
 
+// Load environment variables from a secret
 dotenv.config();
 
 const app = express();
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
-app.use(cors());
+const REDDIT_API_BASE = "https://oauth.reddit.com";
 
-// Set up session
+// Set up CORS for handling cross-origin requests
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET"],
+    credentials: true,
+  })
+);
+
+// Initialize session middleware for handling user sessions
 app.use(
   session({
     secret: "some-secret",
@@ -22,9 +33,11 @@ app.use(
   })
 );
 
+// Initialize passport middleware for handling authentication
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Define Reddit OAuth2 for passport
 passport.use(
   new RedditStrategy(
     {
@@ -33,32 +46,66 @@ passport.use(
       callbackURL: "http://localhost:5000/auth/reddit/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      // For now, let's just return the profile
+      profile.token = accessToken; // Attach token to user profile for later use
       return done(null, profile);
     }
   )
 );
 
+// Serialize user instance to the session
 passport.serializeUser((user, done) => {
   done(null, user);
 });
 
+// Deserialize user instance from the session
 passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
 
-app.get("/auth/reddit", passport.authenticate("reddit"));
+// Endpoint for Reddit authentication (begins OAuth2 flow)
+app.get(
+  "/auth/reddit",
+  passport.authenticate("reddit", {
+    scope: ["identity", "mysubreddits"], // Request access to user's identity and subreddits
+  })
+);
 
+// Callback endpoint for Reddit to redirect after user authentication
 app.get(
   "/auth/reddit/callback",
   passport.authenticate("reddit", { failureRedirect: "/" }),
   (req, res) => {
-    // Successful authentication, redirect home.
     console.log("Authenticated Successfully from server.ts");
-    res.redirect("/");
+    res.redirect("http://localhost:3000");
   }
 );
 
+// Endpoint to fetch top 10 subreddits the authenticated user is subscribed to
+app.get("/get-user-subreddits", async (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ message: "Not authenticated!" });
+  }
+
+  const response = await fetch(
+    `${REDDIT_API_BASE}/subreddits/mine/subscriber?limit=10`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${req.user.token}`,
+        "User-Agent": "EthosPoC/0.0.1",
+      },
+    }
+  );
+
+  if (response.ok) {
+    const data = await response.json();
+    res.json(data);
+  } else {
+    res.status(500).json({ message: "Failed to retrieve subreddits!" });
+  }
+});
+
+// Basic endpoint to check authentication status
 app.get("/", (req, res) => {
   if (req.isAuthenticated()) {
     res.json({ message: "You are authenticated!", user: req.user });
@@ -67,6 +114,7 @@ app.get("/", (req, res) => {
   }
 });
 
+// Start the server
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import React, { useEffect, useState } from "react";
 
 function App() {
   const [message, setMessage] = useState("");
+  const [subreddits, setSubreddits] = useState([]);
 
   useEffect(() => {
+    // Get the welcome message from server
     fetch("http://localhost:5000/")
       .then((response) => response.json())
       .then((data) => setMessage(data.message));
+
+    // Try fetching the user's subreddits if they're authenticated
+    fetch("http://localhost:5000/get-user-subreddits", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.data && data.data.children) {
+          setSubreddits(
+            data.data.children.map((child) => child.data.display_name_prefixed)
+          );
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching subreddits:", error);
+      });
   }, []);
 
   return (
@@ -14,6 +33,12 @@ function App() {
       <h1>Reddit Auth</h1>
       <p>{message}</p>
       <a href="http://localhost:5000/auth/reddit">Login with Reddit</a>
+      <h2>Your Subreddits:</h2>
+      <ul>
+        {subreddits.map((subreddit) => (
+          <li key={subreddit}>{subreddit}</li>
+        ))}
+      </ul>
     </div>
   );
 }
